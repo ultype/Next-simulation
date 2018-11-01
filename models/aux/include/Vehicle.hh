e4582ec2319844b1ba29e4c9c616020032628163
Chun Hsu Lai <as2266317@hotmail.com>
2018-11-01 20:36:25 +0800

    Add Data_exchang class
    Data_exchang is for date exchanging between of module, this class use
    std::map to store the information that module want to exchang, in this
    simulator there are 4 types data that will be used for exchang, so that
    Data_exchang has 4 maps to store these data, use can access data by
    hget() function, and save data by hset.

--- models/aux/include/Vehicle.hh
+++ models/aux/include/Vehicle.hh
@@ -2,6 +2,9 @@
 #define Vehicle_list__HH
 #include "aux.hh"
 #include <vector>
+#include <map>
+#include <string>
+#include <armadillo>
 
 class FH_module {
  public:
@@ -11,6 +14,28 @@ class FH_module {
   virtual void algorithm(double int_step) = 0;
   virtual void init() = 0;
   virtual void update_diagnostic_attributes(double int_step) = 0;
+};
+
+class Data_exchang {
+  public :
+    Data_exchang(){};
+    ~Data_exchang(){};
+
+    void hset(std::string input, int val) { int_table[input] = val; }
+    void hset(std::string input, double val) { double_table[input] = val; }
+    void hset(std::string input, arma::vec val) { vec_table[input] = val; }
+    void hset(std::string input, arma::mat val) { mat_table[input] = val; }
+
+    void hget(std::string input, int *val) { *val = int_table.find(input)->second; }
+    void hget(std::string input, double *val) { *val = double_table.find(input)->second; }
+    void hget(std::string input, arma::vec val) { val = vec_table.find(input)->second; }
+    void hget(std::string input, arma::mat val) { val = mat_table.find(input)->second; }
+
+  private :
+    std::map<std::string, int> int_table;
+    std::map<std::string, double> double_table;
+    std::map<std::string, arma::vec> vec_table;
+    std::map<std::string, arma::mat> mat_table;
 };
 
 // class Vehicle {
