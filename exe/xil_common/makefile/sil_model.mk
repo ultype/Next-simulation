e9bf0cd29f84e0559cbae11dc0e3d9d67f90058e
Chun Hsu Lai <as2266317@hotmail.com>
2018-12-06 09:47:10 +0800

    Remove boost library

--- exe/xil_common/makefile/sil_model.mk
+++ exe/xil_common/makefile/sil_model.mk
@@ -15,5 +15,5 @@ TRICK_CFLAGS += --std=gnu11 ${INCLUDES} -g -D_GNU_SOURCE -DCONFIG_SIL_ENABLE
 TRICK_CFLAGS += -Wall -Wmissing-prototypes -Wextra -Wshadow
 TRICK_CXXFLAGS += --std=c++17 ${INCLUDES} -g -DCONFIG_SIL_ENABLE
 TRICK_CXXFLAGS += -Wall -Wextra -Wshadow
-TRICK_USER_LINK_LIBS += -larmadillo -lboost_serialization -lhiredis
+TRICK_USER_LINK_LIBS += -larmadillo -lhiredis
 MAKEFLAGS += -j16
