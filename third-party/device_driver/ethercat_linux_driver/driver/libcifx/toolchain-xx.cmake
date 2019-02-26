0fb59d02b2f88e813792a486d7fcab7242f77c11
ultype1993 <ultype1993@gmail.com>
2019-02-26 16:49:50 +0800

    Add CIFX_LIBS in cmm_flags.mk

--- third-party/device_driver/ethercat_linux_driver/driver/libcifx/toolchain-xx.cmake
+++
@@ -1,9 +0,0 @@
-
-# TODO: specify target system
-SET(CMAKE_SYSTEM_NAME Linux)
-
-# TODO: specify the cross compiler
-# e.g. SET(CMAKE_C_COMPILER  /opt/mycrosscompiler/gcc)
-SET(CMAKE_C_COMPILER   )
-SET(CMAKE_CXX_COMPILER )
-
